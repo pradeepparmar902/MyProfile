d3c65aeb52fe92ebc64946ff0df5f067e52cb78b
Pradeep Parmar <pradeep.parmar@wppmedia.com>
2026-06-15 23:38:45 +0530

    fix: bind server.js to 0.0.0.0 to fix 503 reverse proxy connection errors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const hostname = process.env.HOSTNAME || '0.0.0.0';
 const port = process.env.PORT || 3000;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
